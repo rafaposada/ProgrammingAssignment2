7fcf1f2a39b4e7b3814df436b73f2fdb403f5656
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    In the assignment I changed variable m to i so it's clear that it stores an inverse matrix and not a mean. I also changed the function mean for solve and the variable x was forced to be a matrix instead of a numeric value

---
+++ Tarea1.R
@@ -0,0 +1,25 @@
+makeMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheinverse <- function(x, ...) {
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached inverse matrix")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setmean(i)
+  i
+}
